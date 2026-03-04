d6db6a1adb07aeb6647973bbf5e10fa0fa1b9a80
s.benedetti <s.benedetti@donatonimacchine.eu>
2026-03-04 11:36:14 +0100

    fix

--- DP/login.js
+++ DP/login.js
@@ -1,35 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const pageName = window.location.pathname.split("/").pop();
-    sessionStorage.setItem("pageName", pageName);
+  const pageName = window.location.pathname.split("/").pop();
+  sessionStorage.setItem("pageName", pageName);
 
-    if (sessionStorage.getItem("isLoggedIn") !== "true")         
-    {
-        const fragment  = window.location.hash.substring(1); // Prende il valore dopo #
-        const [parametro1] = fragment.split("|"); // Divide i valori
-        const output = document.getElementById("parametro");        
+  if (sessionStorage.getItem("isLoggedIn") !== "true")
+  {
+      const fragment  = window.location.hash.substring(1);
+      const [parametro1] = fragment.split("|");
+      const output = document.getElementById("parametro");
 
-        fetch(`http://192.168.6.14:8081/api/evaluate?token=${encodeURIComponent(parametro1)}`)
-  .then(response => {
-    if (!response.ok) throw new Error("Errore nella risposta HTTP");
-    return response.json(); // supponiamo che la risposta sia `true` o `false` in JSON
-  })
-  .then(text => {
- 
-    if (text == "True"){ 
-    //RICHIESTA IN OK
-    console.info("login ok");
-    sessionStorage.setItem("isLoggedIn", "true");   
-    }
-    else {
-    //RICHIESTA IN KO
-    console.info("login KO");
-    window.location.href = "login.html"; // Torna alla pagina di login se non autenticato
-    }
-  })
-  .catch(error => {
-    console.error("Errore:", error);
-  });
+      // crea iframe invisibile
+      const iframe = document.createElement("iframe");
+      iframe.style.display = "none";
 
-    }    
+      iframe.src = `https://softwaredonatoni.github.io/DSCEV/#${encodeURIComponent(parametro1)}`;
+
+      iframe.onload = function () {
+
+          try {
+
+              const result = iframe.contentDocument.getElementById("out").textContent.trim();
+
+              if (result == "true" || result == "True")
+              {
+                  console.info("login ok");
+                  sessionStorage.setItem("isLoggedIn", "true");
+              }
+              else
+              {
+                  console.info("login KO");
+                  window.location.href = "login.html";
+              }
+
+          }
+          catch (error)
+          {
+              console.error("Errore:", error);
+          }
+
+      };
+
+      document.body.appendChild(iframe);
+  }
+
 });
