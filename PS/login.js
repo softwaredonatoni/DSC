e0e9bd9cba4d2f915a2e7045d8a6566bfbc34fe1
Software Admin <admin.software@donatonigroup.local>
2026-03-04 17:03:36 +0100

    Deploy request: R_PS_20260304170331158

--- PS/login.js
+++ PS/login.js
@@ -3,33 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const pageName = window.location.pathname.split("/").pop();
     sessionStorage.setItem("pageName", pageName);
 
-    if (sessionStorage.getItem("isLoggedIn") !== "true")         
-    {
-        const fragment  = window.location.hash.substring(1); // Prende il valore dopo #
-        const [parametro1] = fragment.split("|"); // Divide i valori
-        const output = document.getElementById("parametro");        
+    if (sessionStorage.getItem("isLoggedIn") !== "true") {
+        const fragment = window.location.hash.substring(1);
+        const [parametro1] = fragment.split("|");
+        let result = 'false';
+        const token = 'Donatoni2026!'
 
-        fetch(`http://192.168.6.14:8081/api/evaluate?token=${encodeURIComponent(parametro1)}`)
-  .then(response => {
-    if (!response.ok) throw new Error("Errore nella risposta HTTP");
-    return response.json(); // supponiamo che la risposta sia `true` o `false` in JSON
-  })
-  .then(text => {
- 
-    if (text == "True"){ 
-    //RICHIESTA IN OK
-    console.info("login ok");
-    sessionStorage.setItem("isLoggedIn", "true");   
-    }
-    else {
-    //RICHIESTA IN KO
-    console.info("login KO");
-    window.location.href = "login.html"; // Torna alla pagina di login se non autenticato
-    }
-  })
-  .catch(error => {
-    console.error("Errore:", error);
-  });
 
-    }    
+        if (parametro1 == token) {
+            result = 'True';
+        }
+
+        if (result == "True") {
+            console.info("login ok");
+            sessionStorage.setItem("isLoggedIn", "true");
+        }
+        else {
+            console.info("login KO");
+            window.location.href = "login.html";
+        }
+
+
+    }
+
 });
