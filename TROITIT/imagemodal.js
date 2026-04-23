ce6cbff47ea05c83e0bc1888a58f191d880efcaf
s.benedetti <s.benedetti@donatonimacchine.eu>
2026-04-23 11:03:36 +0200

    fix

--- TROITIT/imagemodal.js
+++
@@ -1,8 +0,0 @@
-function showModal(src) {
-    document.getElementById("modal-img").src = src;
-    document.getElementById("modal").style.display = "flex";
-  }
-
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
-  }
